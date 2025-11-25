7d092adb39c661f37bcc9152519bdea17dac6e6b
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-11-25 19:03:35 +0100

    doc: rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
     "sphinxcontrib.bibtex",
+    "sphinx_rtd_theme",
 ]
 
 # templates_path = ["_templates"]
@@ -52,5 +53,5 @@ bibtex_bibfiles = ["references.bib"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 # html_static_path = ["_static"]
